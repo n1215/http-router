51e235df91019bb2ddbb017bdb40cec86177db64
n1215 <knakae1215@gmail.com>
2017-12-05 01:20:53 +0900

    fix wrong signature

--- src/RoutingErrorResponderInterface.php
+++ src/RoutingErrorResponderInterface.php
@@ -3,7 +3,9 @@ declare(strict_types=1);
 
 namespace N1215\Http\Router;
 
+use Psr\Http\Message\ResponseInterface;
+
 interface RoutingErrorResponderInterface
 {
-    public function respond(RoutingErrorInterface $error): int;
+    public function respond(RoutingErrorInterface $error): ResponseInterface;
 }
