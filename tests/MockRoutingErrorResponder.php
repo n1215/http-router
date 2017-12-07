8c46342b27f825399cf8cea0c9837dafbc24f26f
n1215 <knakae1215@gmail.com>
2017-12-07 17:45:47 +0900

    fix failed test

--- tests/MockRoutingErrorResponder.php
+++ tests/MockRoutingErrorResponder.php
@@ -4,11 +4,12 @@ declare(strict_types=1);
 namespace N1215\Http\Router;
 
 use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
 use Zend\Diactoros\Response\TextResponse;
 
 class MockRoutingErrorResponder implements RoutingErrorResponderInterface
 {
-    public function respond(RoutingErrorInterface $error): ResponseInterface
+    public function respond(ServerRequestInterface $request, RoutingErrorInterface $error): ResponseInterface
     {
         return new TextResponse($error->getMessage(), $error->getStatusCode());
     }
